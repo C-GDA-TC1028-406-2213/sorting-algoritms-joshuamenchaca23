89016c022b6fd1c57a1e9ac5fcbdf6b2e987e6e6
Joshua <a01641943@tec.mx>
2025-09-03 21:18:09 -0600

    Actualicé el archivo con las correcciones

--- src/sorting.cpp
+++ src/sorting.cpp
@@ -8,32 +8,117 @@
 namespace SortingAlgorithms {
 
 void bubbleSort(int arr[], int n) {
-    
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if ((arr[j] > arr[j + 1])) {
+                int temporal= arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]=temporal;
+            }
+        }
+    }
 }
+
 
 void selectionSort(int arr[], int n) {
-    
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        
+        int temp = arr[i];
+        arr[i] = arr[min_idx];
+        arr[min_idx] = temp;
+    }
 }
+
 
 void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int temp1 = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > temp1) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp1;
+    }
     
 }
 
-void merge(int arr[], int left, int mid, int right) {
-    
+void merge(int arr[], int l, int mid, int r) {
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+    delete[] L;
+    delete[] R;
 }
 
-void mergeSort(int arr[], int left, int right) {
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int mid = l + (r - l) / 2;
+
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+
+        merge(arr, l, mid, r); // Llama a merge aquí
+    }
     
 }
 
 int partition(int arr[], int low, int high) {
-    
-    return 0;
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
 }
 
 void quickSort(int arr[], int low, int high) {
-    
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
 
 void heapify(int arr[], int n, int i) {
